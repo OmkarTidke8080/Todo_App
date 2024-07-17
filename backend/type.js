116fa696e530f3fcf8b0ced8547ff214cc388e12
OmkarTidke8080 <omkartidke8080@gmail.com>
2024-07-17 11:56:15 +0530

    type.js file added

---
+++ backend/type.js
@@ -0,0 +1,21 @@
+import zod from "zod";
+
+export const createTodo = zod.object({
+  title: zod.string(),
+  description: zod.string(),
+});
+
+export const updateTodo = zod.object({
+  id: zod.string(),
+});
+
+/*
+{
+title : string,
+description:string
+}
+{
+id = string,
+}
+
+*/
